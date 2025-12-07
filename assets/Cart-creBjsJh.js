fc3ad21318fe48eaed56ddf93981d4900648d0c3
OmarHade <omarhade575@gmail.com>
2025-12-07 13:22:21 +0200

    Updates

---
+++ assets/Cart-creBjsJh.js
@@ -0,0 +1,6 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/FullCart-BGFMefXO.js","assets/index-Q_c4-lBm.js","assets/index-DQsYWP-w.css","assets/EmptyCart-CtIYMrPY.js"])))=>i.map(i=>d[i]);
+import{j as t,u as n,r as o,R as e,_ as s}from"./index-Q_c4-lBm.js";function l({quantity:r}){return t.jsx(t.Fragment,{children:t.jsxs("h1",{className:"text-3xl md:max-lg:text-2xl text-red font-bold",children:["Your Cart (",r,")"]})})}const x=e.lazy(()=>s(()=>import("./FullCart-BGFMefXO.js"),__vite__mapDeps([0,1,2]))),i=e.lazy(()=>s(()=>import("./EmptyCart-CtIYMrPY.js"),__vite__mapDeps([3,1,2])));function d(){const{cart:r,getQuantity:a}=n();return t.jsx(t.Fragment,{children:t.jsxs("section",{className:`md:w-1/3 lg:1/4\r
+                bg-rose-50\r
+            p-6 my-4\r
+            rounded-xl\r
+            relative`,children:[t.jsx(l,{quantity:a()}),t.jsx(o.Suspense,{children:r.length>0?t.jsx(x,{}):t.jsx(i,{})})]})})}export{d as default};
